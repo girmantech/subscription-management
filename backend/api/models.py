cbcfb1e4a5951567552e749e8ae42612ce10b09f
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-28 12:39:35 +0530

    added customer, currency and product models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=320)
+    created_at = models.BigIntegerField()
+
+    def __str__(self):
+        return self.code
+
+
+class Customer(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=320)
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=320)
+    currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=12)
+    created_at = models.BigIntegerField()
+    deleted_at = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    created_at = models.BigIntegerField()
+    deleted_at = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
