927567a9b35342a9fe572ac443e38a0bf4aac091
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-09-30 09:53:03 +0530

    renamed "backend" directory to "subscription-management" also added product_pricing table model

--- subscription-management/api/admin.py
+++ subscription-management/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Currency, Customer, Product
+from .models import Currency, Customer, Product, ProductPricing
 
 # Register your models here.
 admin.site.register(Currency)
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(ProductPricing)
--- subscription-management/api/models.py
+++ subscription-management/api/models.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.constraints import ExclusionConstraint
+from django.contrib.postgres.fields import DateRangeField
 from django.db import models
 
 # Create your models here.
@@ -38,4 +40,26 @@ class Product(models.Model):
         return self.name
 
 
+class ProductPricing(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    price = models.IntegerField()
+    currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
+    created_at = models.BigIntegerField()
+    deleted_at = models.BigIntegerField(null=True, blank=True)
+    class Meta:
+        constraints = [
+            ExclusionConstraint(
+                name="unique_price_in_interval",
+                expressions=[
+                    (models.F("product"), "="),
+                    (models.F("currency"), "="),
+                    (DateRangeField("from_date", "to_date", "[]"), "&&")
+                ],
+                condition=models.Q(deleted_at__isnull=True)
+            )
+        ]
 
+    def __str__(self):
+        return f"{self.product.name} - {self.currency.code} ({self.from_date} to {self.to_date})"
