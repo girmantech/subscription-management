09df1d55676bf1ac4f1049b462c2f935d905bda0
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-10-05 11:59:34 +0530

    added remaining table models with manual migration file for exclusion contraints

--- subscription-management/api/admin.py
+++ subscription-management/api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Currency, Customer, Product, ProductPricing
+from .models import Currency, Customer, Product, ProductPricing, Plan, Invoice, Subscription
 
 # Register your models here.
 admin.site.register(Currency)
 admin.site.register(Customer)
+admin.site.register(Invoice)
+admin.site.register(Plan)
 admin.site.register(Product)
 admin.site.register(ProductPricing)
+admin.site.register(Subscription)
--- subscription-management/api/models.py
+++ subscription-management/api/models.py
@@ -1,5 +1,3 @@
-from django.contrib.postgres.constraints import ExclusionConstraint
-from django.contrib.postgres.fields import DateRangeField
 from django.db import models
 
 # Create your models here.
@@ -41,25 +39,84 @@ class Product(models.Model):
 
 
 class ProductPricing(models.Model):
+    id = models.BigAutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    from_date = models.DateField()
-    to_date = models.DateField()
+    from_date = models.BigIntegerField()
+    to_date = models.BigIntegerField()
     price = models.IntegerField()
     currency = models.ForeignKey(Currency, on_delete=models.RESTRICT)
+    tax_percentage = models.FloatField()
     created_at = models.BigIntegerField()
     deleted_at = models.BigIntegerField(null=True, blank=True)
-    class Meta:
-        constraints = [
-            ExclusionConstraint(
-                name="unique_price_in_interval",
-                expressions=[
-                    (models.F("product"), "="),
-                    (models.F("currency"), "="),
-                    (DateRangeField("from_date", "to_date", "[]"), "&&")
-                ],
-                condition=models.Q(deleted_at__isnull=True)
-            )
-        ]
 
     def __str__(self):
         return f"{self.product.name} - {self.currency.code} ({self.from_date} to {self.to_date})"
+
+
+class Plan(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    billing_interval = models.IntegerField(default=1)
+    created_at = models.BigIntegerField()
+    deleted_at = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.billing_interval} Month(s)"
+
+
+class Invoice(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
+    plan = models.ForeignKey(Plan, on_delete=models.RESTRICT)
+    tax_amount = models.IntegerField()
+    total_amount = models.IntegerField()
+    
+    class InvoiceStatus(models.TextChoices):
+        DRAFT = "DRAFT"
+        PAID = "PAID"
+        UNPAID = "UNPAID"
+    
+    status = models.CharField(max_length=7, choices=InvoiceStatus.choices, default=InvoiceStatus.UNPAID)
+
+    due_date = models.BigIntegerField()
+    paid_at = models.BigIntegerField(blank=True, null=True)
+    created_at = models.BigIntegerField()
+    deleted_at = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Invoice {self.id} - {self.customer.name} - {self.plan.product.name} - {self.plan.billing_interval} Month(s) - Total: {self.total_amount}"
+    
+
+class Subscription(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    class SubscriptionStatus(models.TextChoices):
+        ACTIVE = "ACTIVE"
+        INACTIVE = "INACTIVE"
+        UPGRADED = "UPGRADED"
+    
+    status = models.CharField(max_length=9, choices=SubscriptionStatus.choices, default=SubscriptionStatus.INACTIVE)
+
+    invoice = models.ForeignKey(Invoice, on_delete=models.DO_NOTHING, related_name="subscriptions")
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="subscriptions")
+    starts_at = models.BigIntegerField()
+    ends_at = models.BigIntegerField()
+    renewed_at = models.BigIntegerField(blank=True, null=True)
+    renewed_subscription = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True, related_name="renewed_from"
+    )
+    downgraded_at = models.BigIntegerField(blank=True, null=True)
+    downgraded_to_plan = models.ForeignKey(
+        Plan, on_delete=models.SET_NULL, blank=True, null=True, related_name="downgraded_subscriptions"
+    )
+    upgraded_at = models.BigIntegerField(blank=True, null=True)
+    upgraded_to_plan = models.ForeignKey(
+        Plan, on_delete=models.SET_NULL, blank=True, null=True, related_name="upgraded_subscriptions"
+    )
+
+    cancelled_at = models.BigIntegerField(blank=True, null=True)
+    created_at = models.BigIntegerField(blank=True, null=True)
+    deleted_at = models.BigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Subscription {self.id} for Invoice {self.invoice.id} - Status: {self.status}"
